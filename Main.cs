a91447a3265d8adc601b9b994514e693124c350b
Fl1p <nfl1pg@gmail.com>
2021-11-17 21:15:22 +0600

    mono idk

---
+++ Main.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using BepInEx;
+using UnityEngine;
+
+namespace FPSCounter
+{
+    [BepInPlugin("xyz.fl1pnatic.monofpscounter", "FPS Counter", "1.0.0.0")]
+    public class Main : BaseUnityPlugin
+    {
+
+        public void Start()
+        {
+            Debug.Log("Loaded FPS Counter!");
+        }
+
+        public void OnGUI()
+        {
+            float fps = 1 / Time.unscaledDeltaTime;
+            GUI.Label(new Rect(Screen.width - Screen.width + 10, Screen.height - Screen.height + 20, 100, 200), "FPS: " + fps);
+        }
+    }
+}
